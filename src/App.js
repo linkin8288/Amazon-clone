f1fc0fe49433cca3f885675a5876aa1124ad1312
Charlie <59826303+linkin8288@users.noreply.github.com>
2022-06-23 08:03:08 +0200

    Deploy Website

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import Header from './Components/Header/Header';
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
-
+import Home from './Components/Home/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Router is a parent component, Routes is children which determine
 
 function App() {
   return (
     <Router>
       <div>
-
-      <Header />
-
+        <Header />
+        <Home />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/checkout" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
